0052981ede897976e8bf1b005ed86845df682ae4
Shwetank Dohroo <127093317+ShwetankDohroo1@users.noreply.github.com>
2024-02-05 17:39:04 +0530

    Time: 9 ms (24.52%), Space: 8.9 MB (13.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(int i=0;i<s.length();i++){
+            if(isalnum(s[i])){
+                ans.push_back(tolower(s[i]));
+            }
+        }  
+
+        int l=0,r=ans.length()-1;
+   
+        while(l <= r){
+            if(ans[l] != ans[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+};
