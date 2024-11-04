55f4817be3742854a0f24b36b423c7af39f6e7a7
Shwetank Dohroo <127093317+ShwetankDohroo1@users.noreply.github.com>
2024-11-04 20:27:14 +0530

    Time: 2 ms (39.51%), Space: 41.6 MB (39.10%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,21 +1,23 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        Boolean[] dp1 = new Boolean[366];
-        for(int day:days){
-            dp1[day] = true; 
+        Set<Integer> set = new HashSet<>();
+        for(int i:days){
+            set.add(i);
         }
-        int[] dp = new int[366];
+        int ans = days[days.length -1];
+        int[] dp = new int[ans + 1];
         dp[0] = 0;
-        for(int i=1;i<=365;i++){
-            if(dp1[i] == null || !dp1[i]){
+        for(int i=1;i<=ans;i++){
+            if(!set.contains(i)){
                 dp[i] = dp[i-1];
             }
             else{
-                dp[i] = Math.min(dp[i - 1] + costs[0],
-                        Math.min(dp[Math.max(0, i - 7)] + costs[1],
-                        dp[Math.max(0, i - 30)] + costs[2]));
+                int day1 = dp[i-1] + costs[0];
+                int day7 = dp[Math.max(0,i-7)] + costs[1];
+                int day30 = dp[Math.max(0,i-30)]  +costs[2];
+                dp[i] = Math.min(day1,Math.min(day7,day30));
             }
         }
-        return dp[365];
+        return dp[ans];
     }
 }
