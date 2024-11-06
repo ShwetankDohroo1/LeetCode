fb05c3ec94dc6db151ff6d5d171281945458ed92
Shwetank Dohroo <127093317+ShwetankDohroo1@users.noreply.github.com>
2024-11-06 09:18:51 +0530

    Time: 0 ms (100.00%), Space: 40 MB (89.63%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,14 @@
 class Solution {
     public int fib(int n) {
         int[] dp = new int[n+1];
-        for(int i=0;i<dp.length;i++){
-            dp[i] = -1;
+        dp[0] = 0;
+        if(n == 0){
+            return dp[0];
         }
-        return help(n,dp);
-    }
-    public int help(int n,int[] dp){
-        if(n == 0 || n == 1){
-            return n;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = help(n-1,dp) + help(n-2,dp);
         return dp[n];
     }
 }
