9b94bdef384b470941bfdae0a9be23340584abd2
Shwetank Dohroo <127093317+ShwetankDohroo1@users.noreply.github.com>
2025-01-23 18:46:43 +0530

    Time: 3 ms, Memory: 52.8 MB - LeetHub

--- 1621-96-1267-count-servers-that-communicate/1621-96-1267-count-servers-that-communicate.java
+++ 1621-96-1267-count-servers-that-communicate/1621-96-1267-count-servers-that-communicate.java
@@ -1,38 +1,25 @@
 class Solution {
     public int countServers(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] vis = new boolean[m][n];
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] row = new int[n];
+        int[] col = new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
         int ans = 0;
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    int count = dfs(i,j,vis,grid);
-                    if(count > 1) { 
-                        ans += count; 
-                    }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)){
+                    ans++;
                 }
             }
         }
         return ans;
-    }
-
-    public int dfs(int i, int j, boolean[][] vis, int[][] grid){
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-        vis[i][j] = true;
-        for(int k = 0;k<n;k++){
-            if(grid[i][k] == 1 && !vis[i][k]){
-                count+= dfs(i,k,vis,grid);
-            }
-        }
-        for(int k=0;k<m;k++){
-            if(grid[k][j] == 1 && !vis[k][j]){
-                count+=dfs(k,j,vis,grid);
-            }
-        }
-        return count + 1;
     }
 }
